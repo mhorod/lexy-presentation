faf335795172d3d0c4baaf337cc88174a65f68ad
mhorod <michalhorodecki2002@gmail.com>
2022-11-20 22:35:05 +0100

    base for advanced api example

---
+++ 2_advanced_api.cpp
@@ -0,0 +1,18 @@
+// Examples of a bit more advanced lexy functionality
+
+#include <iostream>
+
+#include <lexy/action/parse.hpp>
+#include <lexy/callback.hpp>
+#include <lexy/dsl.hpp>
+#include <lexy/input/string_input.hpp>
+#include <lexy_ext/report_error.hpp>
+
+namespace grammar
+{
+
+}
+
+int main()
+{
+}
